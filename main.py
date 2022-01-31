475f86dd8dbdcbddf44601542c6578cb2142b48e
Mike MacHenry <mike.machenry@gmail.com>
2022-01-31 13:01:52 -0600

    Updating code to run for first test in bus

--- main.py
+++ main.py
@@ -21,9 +21,24 @@ YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 SOFTWHITE = (255, 200, 100)
 
-#https://jjmojojjmojo.github.io/time-based-fading.html below class 1 loop fader
 
-rgb = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=1.0, auto_write=False)
+light_mode = 0
+previous_mode_pin = False #prior consecutive assessment of button held down or not
+
+
+#BUTTON SELECT
+def handle_mode_button ():
+    if mode_pin.value == True and previous_mode_pin == False: #implied True if mode_pin.value: and false if not preveious_mode_pin
+                                                              #this is a rising edge detector
+        light_mode += 1
+        if light_mode == 8:
+            light_mode = 0
+           
+    previous_mode_pin = mode_pin.value
+           
+           
+#RAINBOW FADE
+#https://jjmojojjmojo.github.io/time-based-fading.html below class 1 loop fader
 
 class LoopFader:
     def __init__(self, palette):
@@ -40,36 +55,31 @@ class LoopFader:
 
 pride = (4980736, 4980736, 4981248, 4982272, 4984064, 4986880, 4990720, 4996096, 3951616, 1592320, 412672, 19456, 13312, 5126, 1048, 60, 76, 65612, 327756, 852044, 1507367, 2359309, 3538946, 4980736)
 
-
 fader = LoopFader(pride)
+#end of loop fader
+
+
 
 while True:
-    rgb.fill(fader.color)
-    rgb.write()
-    time.sleep(0.1)
-
-light_mode = 0
-previous_mode_pin = False #prior consecutive assessment of button held down or not
-
-def handle_mode_button ():
-    if mode_pin.value == True and previous_mode_pin == False: #implied True if mode_pin.value: and false if not preveious_mode_pin
-                                                              #this is a rising edge detector
-        light_mode += 1
-        if light_mode == 7:
-            light_mode = 0
-           
-    previous_mode_pin = mode_pin.value
-           
-           
-
-while True:
-    pixels.fill(SOFTWHITE)  
-    pixels.show()
-    print(analog_in.value)
-    time.sleep(0.1)
-    pixels.brightness = analog_in.value / 65536    #potentiometer
-
-   
-    #momentary switch (clicker button that changes light color)
     handle_mode_button()
-
+    pixel.brightness = 1.0
+    #pixels.brightness = analog_in.value / 65536    #potentiometer
+    if light_mode == 0:
+         pixels.fill(SOFTWHITE)
+    elif light_mode == 1:
+         pixels.fill(RED)
+    elif light_mode == 2:
+         pixels.fill(BLUE)
+    elif light_mode == 3:
+         pixels.fill(TEAL)
+    elif light_mode == 4:
+         pixels.fill(GREEN)
+    elif light_mode == 5:
+         pixels.fill(YELLOW)
+    elif light_mode == 6:
+         pixels.fill(WHITE)
+    elif light_mode == 7:
+         pixels.fill(fader.color)
+   
+    pixels.show()
+    time.sleep(0.1)
